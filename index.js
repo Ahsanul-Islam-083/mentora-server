9af91e77abdd4e4980bf2a81bc88961876f0cf06
Ahsanul-Islam-083 <ahsanulislam083@gmail.com>
2026-05-18 18:38:20 +0600

    feat: implement JWT authentication and add enrollment management endpoints with Vercel deployment configuration

--- index.js
+++ index.js
@@ -1,12 +1,15 @@
 const express = require('express');
+const cors = require('cors');
 const dotenv = require('dotenv');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
+const { createRemoteJWKSet, jwtVerify } = require('jose-cjs');
 dotenv.config();
 const app = express();
 const port = process.env.PORT || 8080;
 
 const uri = process.env.MONGODB_URI;
-
+app.use(cors());
+app.use(express.json());
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
@@ -15,6 +18,31 @@ const client = new MongoClient(uri, {
         deprecationErrors: true,
     }
 });
+
+const JWKS = createRemoteJWKSet(
+    new URL(`${process.env.CLIENT_URL}/api/auth/jwks`)
+)
+
+const verifyToken = async (req, res, next) => {
+    const { authorization } = req.headers;
+    const token = authorization.split(' ')[1];
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    try {
+
+        const { payload } = await jwtVerify(token, JWKS)
+        // return payload
+        req.user = payload;
+        // console.log(req.user)
+        next();
+    } catch (error) {
+        console.error('Token validation failed:', error)
+        return res.status(403).json({ message: "Forbidden" })
+    }
+
+}
+
 
 async function run() {
     try {
@@ -25,25 +53,85 @@ async function run() {
 
         const db = client.db('mentoradb');
         const coursesCollection = db.collection('courses');
+        const enrollmentCollection = db.collection('enrollments');
 
         app.get('/courses', async (req, res) => {
-            const cursor = coursesCollection.find();
+            const { search } = req.query;
+            let cursor;
+            if (search) {
+                // cursor = coursesCollection.find({ title: {
+                //     $regex : search,
+                //     $options : "i"
+                // }})
+                cursor = coursesCollection.find({
+                    $or: [
+                        {
+                            title: {
+                                $regex: search,
+                                $options: "i"
+                            }
+                        },
+                        {
+                            instructor: {
+                                $regex: search,
+                                $options: "i"
+                            }
+                        }
+                    ]
+                })
+            } else {
+                cursor = coursesCollection.find();
+            }
             const result = await cursor.toArray();
             res.send(result);
-            console.log(result);
+            // console.log(result);
         });
-        
+
         app.get('/featured', async (req, res) => {
             const cursor = coursesCollection.find().limit(4);
             const result = await cursor.toArray();
             res.send(result);
             // console.log(result);
         });
-        app.get('/courses/:coursId', async (req, res) => {
+        app.get('/courses/:coursId', verifyToken, async (req, res) => {
+            // console.log(req.user);
+
             const { coursId } = req.params;
             const result = await coursesCollection.findOne({ _id: new ObjectId(coursId) });
             res.send(result);
         });
+
+        app.get('/enrollments/:userId', verifyToken, async (req, res) => {
+            const { userId } = req.params;
+            const result = await enrollmentCollection.find({ userId: userId }).toArray();
+            res.send(result);
+
+        })
+
+        app.patch("/enrollments/:courseId", verifyToken, async (req, res) => {
+            // console.log("enrolllllll");
+
+            const { courseId } = req.params;
+            const enrollmentData = req.body;
+            // console.log(enrollmentData);
+
+            const course = await coursesCollection.findOne({ _id: new ObjectId(courseId) });
+            if (!course) {
+                return res.status(404).json({ message: "Course Not Found" })
+            }
+            await coursesCollection.updateOne(
+                { _id: new ObjectId(courseId) },
+                { $inc: { enrollCount: 1 }, $set: { lastEnrollesAt: new Date() } }
+            )
+            const result = await enrollmentCollection.insertOne({ ...enrollmentData, enrolledAt: new Date() })
+            res.send(result);
+        })
+
+        app.delete("/enrollments/:id", verifyToken, async (req, res) => {
+            const { id } = req.params;
+            const result = await enrollmentCollection.deleteOne({ _id: new ObjectId(id) });
+            res.send(result);
+        })
 
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
